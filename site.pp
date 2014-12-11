1036a1e52ba0165b7458b4c01d22278ab1bf1141
Abhishek <abhishekb@cybage.com>
2014-12-11 18:20:43 +0530

    adding sql and pp file

---
+++ site.pp
@@ -0,0 +1,24 @@
+#class { '::mysql::server':
+#   override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+#}
+
+#exec{'Followup scripts':
+#        command=>"/usr/bin/mysql < /tmp/mywar/Dump_test.sql",
+#}
+
+package{'tomcat7':
+        ensure=>installed,
+        before=>Service['tomcat'],
+}
+
+
+service{'tomcat':
+        ensure=>running,
+        require=>Package['tomcat7'],
+        }
+
+file {'/var/lib/tomcat7/webapps/EmployeeApplication.war':
+        source=>"/tmp/mywar/EmployeeApplication.war",
+        require=>Service['tomcat'],
+}
+
